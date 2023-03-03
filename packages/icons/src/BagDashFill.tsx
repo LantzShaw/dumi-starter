cc57d09e2dc208155349298e28d3af385db74b67
Lantz <lantzshaw@163.com>
2023-03-03 16:11:11 +0800

    feat: add new icons

---
+++ packages/icons/src/BagDashFill.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import IconBase, { type IconBaseProps } from './IconBase';
+
+const SvgIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM6 9.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6z"
+    />
+  </svg>
+);
+
+const BagDashFill = React.forwardRef<
+  SVGSVGElement,
+  Omit<IconBaseProps, 'name'>
+>((props, ref) => {
+  return (
+    <IconBase name="BagDashFill" {...props} ref={ref}>
+      <SvgIcon />
+    </IconBase>
+  );
+});
+
+export default BagDashFill;
