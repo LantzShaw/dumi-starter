2a5a466a41f95165f4d72aaf35e292dadc7eccfa
Lantz <lantzshaw@163.com>
2023-03-03 16:23:13 +0800

    revert: revert the BEM creator

--- packages/ui/src/utils/create/bem.ts
+++ packages/ui/src/utils/create/bem.ts
@@ -7,50 +7,50 @@
  * b(['disabled', 'primary']) // 'button button--disabled button--primary'
  */
 
-export type Mod = string | { [key: string]: any }
-export type Mods = Mod | Mod[]
+export type Mod = string | { [key: string]: any };
+export type Mods = Mod | Mod[];
 
 function gen(name: string, mods?: Mods): string {
   if (!mods) {
-    return ''
+    return '';
   }
 
   if (typeof mods === 'string') {
-    return ` ${name}--${mods}`
+    return ` ${name}--${mods}`;
   }
 
   if (Array.isArray(mods)) {
     return mods.reduce<string>(
       (ret: string, item: string) => ret + gen(name, item),
-      ''
-    )
+      '',
+    );
   }
 
   return Object.keys(mods).reduce(
     (ret, key) => ret + (mods[key] ? gen(name, key) : ''),
-    ''
-  )
+    '',
+  );
 }
 
 export function createBEM(name: string) {
   return (el?: Mods, mods?: Mods): Mods => {
     if (el && typeof el !== 'string') {
-      mods = el
-      el = ''
+      mods = el;
+      el = '';
     }
 
-    el = el ? `${name}__${el as string}` : name
+    el = el ? `${name}__${el as string}` : name;
 
     // return `${el}${gen(el, mods)}`
     /**
      * FIXME: when createBEM('text'), we can get results like this: lc_button lc-button__text
      */
-    if (name === el) {
-      return `${el}${gen(el, mods)}`
-    } else {
-      return `${name} ${el}${gen(el, mods)}`
-    }
-  }
+    // if (name === el) {
+    return `${el}${gen(el, mods)}`;
+    // } else {
+    //   return `${name} ${el}${gen(el, mods)}`
+    // }
+  };
 }
 
-export type BEM = ReturnType<typeof createBEM>
+export type BEM = ReturnType<typeof createBEM>;
