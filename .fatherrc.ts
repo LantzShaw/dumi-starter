a99eae6d9358ca935b737f938b3dbc18028f1b21
Lantz <lantzshaw@163.com>
2023-03-10 20:01:11 +0800

    chore: update package building process

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   esm: {
-    input: 'packages',
-    output: 'dist',
+    input: 'packages/ui/src/components/',
+    output: 'es',
+  },
+  cjs: {
+    input: 'packages/ui/src/components/',
+    output: 'lib',
   },
   umd: {
     entry: 'packages/index.tsx',
+    output: 'dist',
   },
 });
