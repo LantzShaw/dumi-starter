ac8cc2db71afa44a6689ed9120b822290d2b0332
Lantz <lantzshaw@163.com>
2023-03-11 10:53:47 +0800

    fix(Notification): fix the prop's type

--- packages/ui/src/components/Notification/index.tsx
+++ packages/ui/src/components/Notification/index.tsx
@@ -1,4 +1,4 @@
 import './styles/index.less';
 
 export { default as Notification } from './Notification';
-export type { NotificationProps } from './Notification.d';
+export type { NotificationProps } from './PropsType';
